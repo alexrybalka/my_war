e9b083a8fd1fdbd38495f558a90a976356aae67c
Alexandr <alex.rybalka.2016@gmail.com>
2018-06-27 20:28:44 +0300

    Second

--- new.py
+++ new.py
@@ -5,7 +5,7 @@ war2 = Warrior()
 
 war_battle = Fight(war1, war2)
 
-arm1 = MixedArmy(20, 30)
-arm2 = MixedArmy(15, 10)
+arm1 = MixedArmy(25, 30)
+arm2 = MixedArmy(20, 10)
 
 bat = ArmyFight(arm1, arm2)
